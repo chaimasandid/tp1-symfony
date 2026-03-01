c0caa44a4309b496ff3a292b49dc653c5ef635e6
chaimasandid <chaima.sandid@gmail.com>
2026-03-01 18:05:48 +0100

    feat: Formulaires Symfony avec ArticleType, validation et messages flash

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ArticleRepository;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
 final class ArticlesController extends AbstractController
 {
 #[Route('/articles', name: 'app_articles')]
@@ -21,19 +23,27 @@ public function index(ArticleRepository $articleRepository): Response
 }
 
 #[Route('/articles/nouveau', name: 'app_article_nouveau')]
-public function nouveau(EntityManagerInterface $em): Response
+public function nouveau(Request $request, EntityManagerInterface $em): Response
 {
     $article = new Article();
-    $article->setTitre('Mon premier article');
-    $article->setContenu('Ceci est le contenu de mon premier article créé avec Doctrine.');
-    $article->setAuteur('Étudiant');
-    $article->setDateCreation(new \DateTime());
-    $article->setPublie(true);
+    
 
-    $em->persist($article);
-    $em->flush();
-
-    return new Response("Article créé avec l'id : " . $article->getId());
+    $form = $this->createForm(ArticleType::class, $article);
+    
+    $form->handleRequest($request);
+    
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($article);
+        $em->flush();
+        
+        $this->addFlash('success', 'Article créé avec succès !');
+        
+        return $this->redirectToRoute('app_articles');
+    }
+    
+    return $this->render('articles/nouveau.html.twig', [
+        'formulaire' => $form->createView(),
+    ]);
 }
 
 #[Route('/articles/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
